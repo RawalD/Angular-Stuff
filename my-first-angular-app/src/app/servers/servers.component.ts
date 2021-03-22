a39afa868bcb7770160999d8b109a6549afa99f3
rawald <diprawal@hotmail.com>
2021-03-22 13:21:27 +0200

    Added components with slight styling

---
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<app-server></app-server>',
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+  h3{
+    color: red;
+  }`]
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
