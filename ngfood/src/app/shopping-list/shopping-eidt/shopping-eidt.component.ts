2f6875149f7fc0200279148695671b8191674fc7
rawald <diprawal@hotmail.com>
2021-03-23 11:16:21 +0200

    NgFood project started, components added

---
+++ ngfood/src/app/shopping-list/shopping-eidt/shopping-eidt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-eidt',
+  templateUrl: './shopping-eidt.component.html',
+  styleUrls: ['./shopping-eidt.component.css']
+})
+export class ShoppingEidtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
